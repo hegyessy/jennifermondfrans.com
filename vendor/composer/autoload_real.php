75770d27bd9ab8e00efd85662e4cc7f34b4b4946
Jason Hegyessy <jason@hegyessy.com>
2021-11-12 09:56:33 -0800

    Updating Kirby to latest
    	modified:   composer.lock
    	modified:   kirby/composer.json
    	modified:   kirby/composer.lock
    	deleted:    kirby/config/config.php
    	modified:   kirby/src/Cms/FileRules.php
    	new file:   kirby/src/Sane/Handler.php
    	new file:   kirby/src/Sane/Sane.php
    	new file:   kirby/src/Sane/Svg.php
    	new file:   kirby/src/Sane/Svgz.php
    	new file:   kirby/src/Sane/Xml.php
    	modified:   kirby/src/Toolkit/File.php
    	modified:   vendor/composer/ClassLoader.php
    	modified:   vendor/composer/InstalledVersions.php
    	modified:   vendor/composer/autoload_classmap.php
    	modified:   vendor/composer/autoload_psr4.php
    	modified:   vendor/composer/autoload_real.php
    	modified:   vendor/composer/autoload_static.php
    	modified:   vendor/composer/installed.json
    	modified:   vendor/composer/installed.php

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitf72ce55cfce21eae45fdcb0af0e536ad
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitf72ce55cfce21eae45fdcb0af0e536ad', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf72ce55cfce21eae45fdcb0af0e536ad', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
